dde7427804e4f6e436291fbe4d31803f32ca21a7
紫月狂魔 <3710pmmm@163.com>
2020-04-14 10:56:28 +0800

    Update Form1.Designer.cs

--- 小学生加减乘除出题器/Form1.Designer.cs
+++ 小学生加减乘除出题器/Form1.Designer.cs
@@ -236,7 +236,7 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "小学生加减乘除出题器 by 李大书";
+            this.Text = "小学生加减乘除出题器 by hackgsl";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
